3210b02afb42ce3067eb22fb689be15b00cbb898
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-10-24 16:06:24 -0600

    Adding comments

--- AbstractVectorSpaceSymPy.py
+++ AbstractVectorSpaceSymPy.py
@@ -1,3 +1,10 @@
+'''
+La implementación de espacios vectoriales abstractos en álgebra lineal puede ser bastante avanzada y 
+específica según el contexto. A continuación, te mostraré un ejemplo simplificado en Python que ilustra el 
+concepto de espacio vectorial abstracto en el contexto de los polinomios. En este ejemplo, crearemos un 
+espacio vectorial de polinomios utilizando la biblioteca SymPy para álgebra simbólica:
+'''
+
 from sympy import symbols, expand, diff, simplify
 
 # Definir las variables simbólicas
@@ -24,3 +31,15 @@ print(f'p1(x) + p2(x) = {result_sum}')
 print(f'p1(x) - p2(x) = {result_diff}')
 print(f'Derivative of p1(x) = {result_derivative}')
 print(f'Simplified p1(x) = {result_simplified}')
+
+
+'''
+En este ejemplo, definimos un espacio vectorial abstracto de polinomios utilizando SymPy. 
+Los polinomios son elementos del espacio vectorial, y podemos realizar operaciones como suma, 
+resta, derivación y simplificación en este espacio.
+
+Esto ilustra cómo los espacios vectoriales abstractos pueden ser implementados en Python 
+utilizando bibliotecas de álgebra simbólica como SymPy. Ten en cuenta que este es un ejemplo 
+simplificado, y en aplicaciones más avanzadas, los espacios vectoriales abstractos pueden 
+involucrar conceptos matemáticos más complejos y estructuras personalizadas.
+'''
