397f6a0bf21ab511ffe207c86cf4089e054118c0
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-11-15 16:38:38 -0600

    Adding vectors

--- OperationsExample5.py
+++ OperationsExample5.py
@@ -1,44 +1,59 @@
 import numpy as np
 
-# Definir tres matrices (2x2)
+# Definir matrices (2x2) y vectores (2,)
 matriz_A = np.array([[1, 2], [3, 4]])
 matriz_B = np.array([[5, 6], [7, 8]])
 matriz_C = np.array([[9, 10], [11, 12]])
 
-# Propiedad Asociativa: (A * B) * C == A * (B * C)
-asociativa_izquierda = np.dot(np.dot(matriz_A, matriz_B), matriz_C)
-asociativa_derecha = np.dot(matriz_A, np.dot(matriz_B, matriz_C))
+vector_x = np.array([2, 3])
+vector_y = np.array([4, 5])
+vector_z = np.array([6, 7])
 
-# Propiedad Distributiva: A * (B + C) == (A * B) + (A * C)
-distributiva_izquierda = np.dot(matriz_A, matriz_B + matriz_C)
-distributiva_derecha = np.dot(matriz_A, matriz_B) + np.dot(matriz_A, matriz_C)
+# Propiedad Asociativa con Matrices: (A * B) * C == A * (B * C)
+asociativa_izquierda_matrices = np.dot(np.dot(matriz_A, matriz_B), matriz_C)
+asociativa_derecha_matrices = np.dot(matriz_A, np.dot(matriz_B, matriz_C))
 
-# Propiedad Conmutativa: A * B == B * A
-conmutativa = np.array_equal(np.dot(matriz_A, matriz_B), np.dot(matriz_B, matriz_A))
+# Propiedad Asociativa con Vectores: (x dot y) dot z == x dot (y dot z)
+asociativa_izquierda_vectores = np.dot(np.dot(vector_x, vector_y), vector_z)
+asociativa_derecha_vectores = np.dot(vector_x, np.dot(vector_y, vector_z))
+
+# Propiedad Distributiva con Matrices: A * (B + C) == (A * B) + (A * C)
+distributiva_izquierda_matrices = np.dot(matriz_A, matriz_B + matriz_C)
+distributiva_derecha_matrices = np.dot(matriz_A, matriz_B) + np.dot(matriz_A, matriz_C)
+
+# Propiedad Distributiva con Vectores: x dot (y + z) == (x dot y) + (x dot z)
+distributiva_izquierda_vectores = np.dot(vector_x, vector_y + vector_z)
+distributiva_derecha_vectores = np.dot(vector_x, vector_y) + np.dot(vector_x, vector_z)
+
+# Propiedad Conmutativa con Vectores: x dot y == y dot x
+conmutativa_vectores = np.dot(vector_x, vector_y) == np.dot(vector_y, vector_x)
 
 # Imprimir resultados
+print("Matrices:")
 print("Matriz A:\n", matriz_A)
-print("\nMatriz B:\n", matriz_B)
-print("\nMatriz C:\n", matriz_C)
+print("Matriz B:\n", matriz_B)
+print("Matriz C:\n", matriz_C)
 
-print("\nPropiedad Asociativa:")
-print("(A * B) * C:\n", asociativa_izquierda)
-print("A * (B * C):\n", asociativa_derecha)
+print("\nPropiedad Asociativa con Matrices:")
+print("(A * B) * C:\n", asociativa_izquierda_matrices)
+print("A * (B * C):\n", asociativa_derecha_matrices)
 
-print("\nPropiedad Distributiva:")
-print("A * (B + C):\n", distributiva_izquierda)
-print("(A * B) + (A * C):\n", distributiva_derecha)
+print("\nPropiedad Distributiva con Matrices:")
+print("A * (B + C):\n", distributiva_izquierda_matrices)
+print("(A * B) + (A * C):\n", distributiva_derecha_matrices)
 
-print("\nPropiedad Conmutativa:")
-print("A * B == B * A:", conmutativa)
+print("\nVectores:")
+print("Vector x:", vector_x)
+print("Vector y:", vector_y)
+print("Vector z:", vector_z)
 
+print("\nPropiedad Asociativa con Vectores:")
+print("(x dot y) dot z:", asociativa_izquierda_vectores)
+print("x dot (y dot z):", asociativa_derecha_vectores)
 
-'''
-En este ejemplo:
+print("\nPropiedad Distributiva con Vectores:")
+print("x dot (y + z):", distributiva_izquierda_vectores)
+print("(x dot y) + (x dot z):", distributiva_derecha_vectores)
 
-La propiedad asociativa se verifica al comparar (A * B) * C con A * (B * C).
-La propiedad distributiva se verifica al comparar A * (B + C) con (A * B) + (A * C).
-La propiedad conmutativa se verifica al comparar A * B con B * A.
-Es importante notar que la propiedad conmutativa no siempre se cumple en la multiplicación de matrices, a diferencia de la propiedad asociativa y distributiva. En este ejemplo, las matrices son pequeñas y específicamente elegidas para cumplir con estas propiedades.
-
-'''
+print("\nPropiedad Conmutativa con Vectores:")
+print("x dot y == y dot x:", conmutativa_vectores)
