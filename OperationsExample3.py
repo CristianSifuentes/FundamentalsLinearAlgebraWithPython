fda6e41cb7bd5813a2d9acee6d94036fd5a7837d
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-11-15 12:50:09 -0600

    Adding information

--- OperationsExample3.py
+++ OperationsExample3.py
@@ -14,3 +14,18 @@ multiplicacion_vector_matriz = np.dot(matriz, vector)
 print("Matriz:\n", matriz)
 print("\nVector:\n", vector)
 print("\nMultiplicación de la matriz por el vector:\n", multiplicacion_vector_matriz)
+
+'''
+Mis disculpas por la confusión. Efectivamente, el error se debe a que las dimensiones de las matrices no son compatibles para la multiplicación en ese orden específico. Para realizar la multiplicación de un vector por una matriz en NumPy, el número de columnas en la matriz debe ser igual al número de elementos en el vector.
+'''
+
+
+'''
+En este ejemplo, la multiplicación np.dot(matriz, vector) es válida porque el número de columnas en la matriz (3) es igual al número de elementos en el vector (3). Este resultado será un nuevo vector cuyos elementos son combinaciones lineales de las filas de la matriz multiplicadas por los elementos del vector.
+
+
+
+
+
+
+'''
