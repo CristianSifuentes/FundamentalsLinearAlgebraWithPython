6c96bed32c8e37672effcff454e982666af22c24
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-11-30 16:10:26 -0600

    Adding information for transposed

--- Resume.py
+++ Resume.py
@@ -9,12 +9,21 @@ b = 3
 c = 2
 #print(f"a.shape: {a.shape}")
 #AttributeError: 'int' object has no attribute 'shape'
+#print(f"len(escalares): {len(a)}")
+
+
+
 
 # Operaciones
 suma = a + b
 resta = a - b
 multiplicacion = c * a
 division = a / b if b != 0 else None
+
+# Operación de Transposición
+# ... (no aplica a escalares)
+#print(f"Transpuesta escalar: {a.T}")
+#AttributeError: 'float' object has no attribute 'T'
 
 # Resultados
 print(f"Suma: {suma}")
@@ -33,6 +42,8 @@ v = np.array([-1, 5])
 c = 2
 
 print(f"u.shape: {u.shape}")
+print(f"len(vector): {len(u)}")
+
 
 
 # Operaciones
@@ -40,10 +51,15 @@ suma_vector = u + v
 resta_vector = u - v
 multiplicacion_escalar = c * v
 
+# Operación de Transposición
+transpuesta_u = u.T  # No tiene efecto en un vector
+
+
 # Resultados
 print(f"Suma de vectores: {suma_vector}")
 print(f"Resta de vectores: {resta_vector}")
 print(f"Multiplicación por escalar: {multiplicacion_escalar}")
+print(f"Transpuesta de u: {transpuesta_u}")
 
 
 
@@ -56,6 +72,7 @@ B = np.array([[-1, 0], [2, 1]])
 c = 2
 
 print(f"A.shape: {A.shape}")
+print(f"len(matriz): {len(A)}")
 
 
 # Operaciones
@@ -64,12 +81,18 @@ resta_matriz = A - B
 multiplicacion_escalar_matriz = c * A
 producto_matrices = np.dot(A, B)
 
+# Operación de Transposición
+transpuesta_A = A.T
+transpuesta_B = B.T
+
+
 # Resultados
 print(f"Suma de matrices:\n{suma_matriz}")
 print(f"Resta de matrices:\n{resta_matriz}")
 print(f"Multiplicación por escalar:\n{multiplicacion_escalar_matriz}")
 print(f"Producto de matrices:\n{producto_matrices}")
-
+print(f"Transpuesta de A:\n{transpuesta_A}")
+print(f"Transpuesta de B:\n{transpuesta_B}")
 
 # -------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------------------
@@ -79,6 +102,7 @@ tensor = np.fromfunction(lambda i, j, k: i + j + k, (3, 3, 3), dtype=int)
 
 tensor.shape
 print(f"tensor.shape: {tensor.shape}")
+print(f"len(tensor): {len(tensor)}")
 
 
 # Operaciones
@@ -86,7 +110,11 @@ suma_tensores = tensor + tensor
 multiplicacion_escalar_tensor = 2 * tensor
 contraccion_tensor = np.sum(tensor, axis=2)
 
+# Operación de Transposición
+transpuesta_tensor = tensor.T
+
 # Resultados
 print(f"Suma de tensores:\n{suma_tensores}")
 print(f"Multiplicación por escalar:\n{multiplicacion_escalar_tensor}")
 print(f"Contracción de tensor:\n{contraccion_tensor}")
+print(f"Transpuesta de tensor:\n{transpuesta_tensor}")
