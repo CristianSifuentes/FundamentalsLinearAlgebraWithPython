8b9d195be43431481cb335c30a9298432ac6c509
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-12-05 12:16:08 -0600

    Creating example for associative, distributive and conmutativa

--- Resume.py
+++ Resume.py
@@ -80,6 +80,7 @@ print(f"Producto Cruz de w y x: {producto_cruz}")
 # Matrices
 A = np.array([[1, 2], [3, 4]])
 B = np.array([[-1, 0], [2, 1]])
+C = np.array([[5, 6], [7, 8]])
 c = 2
 
 print(f"A.shape: {A.shape}")
@@ -102,6 +103,21 @@ producto_punto_matrices = np.sum(A * B)
 # Producto Cruz (en matrices, sería un producto elemento a elemento)
 producto_cruz_matrices = A * B
 
+# 1. Propiedad Asociativa:
+# Verificación de la propiedad asociativa
+left_associative = A.dot(B.dot(C))
+right_associative = (A.dot(B)).dot(C)
+
+# 2. Propiedad Distributiva:
+# Verificación de la propiedad distributiva
+left_distributive = A.dot(B + C)
+right_distributive = A.dot(B) + A.dot(C)
+
+# 3. Propiedad Conmutativa (NO es válida para la multiplicación de matrices):
+product_AB = A.dot(B)
+product_BA = B.dot(A)
+
+
 # Resultados
 print(f"Suma de matrices:\n{suma_matriz}")
 print(f"Resta de matrices:\n{resta_matriz}")
@@ -110,9 +126,23 @@ print(f"Producto de matrices:\n{producto_matrices}")
 print(f"Transpuesta de A:\n{transpuesta_A}")
 print(f"Transpuesta de B:\n{transpuesta_B}")
 
-# Resultados
 print(f"Producto Punto de A y B: {producto_punto_matrices}")
 print(f"Producto Cruz de A y B (elemento a elemento):\n{producto_cruz_matrices}")
+
+# Propiedad Asociativa
+print(f"A · (B · C):\n{left_associative}")
+print(f"(A · B) · C:\n{right_associative}")
+print("¿La propiedad es asociativa?", np.allclose(left_associative, right_associative))
+
+# Propiedad Distributiva
+print(f"A · (B + C):\n{left_distributive}")
+print(f"A · B + A · C:\n{right_distributive}")
+print("¿La propiedad es distributiva?", np.allclose(left_distributive, right_distributive))
+
+# Propiedad Conmutativa (NO es válida para la multiplicación de matrices)
+print(f"A · B:\n{product_AB}")
+print(f"B · A:\n{product_BA}")
+print("¿La propiedad es conmutativa?", np.array_equal(product_AB, product_BA))
 
 # -------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------------------
